b7fcb6f2c6b6aed36b6b181142fc4d8685dded13
biruk-bereka <birukbeletech1212@gmail.com>
2023-01-23 23:22:20 +0300

    todolist features

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import './style.css';
+import Todo from './modules/todoList.js';
+import Completed from './modules/completed.js';
+
+const todoClass = new Todo();
+const completed = new Completed();
+
+const addButton = document.querySelector('.addList');
+const clearButton = document.querySelector('.clear');
+
+addButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const inputValue = document.querySelector('.listInput').value;
+  if (inputValue !== '') todoClass.addList();
+});
+
+clearButton.addEventListener('click', () => {
+  completed.clearCompleted();
+});
+
+todoClass.showList();
